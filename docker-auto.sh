812d6fada551bc90b38093a53c0dcd3f94308f67
Emanuele Disco <emawind84@gmail.com>
2017-03-20 10:53:03 +0900

    update

---
+++ docker-auto.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPT_BASE_PATH"
+
+REGISTRY_URL="$REGISTRY_URL"
+if [ -z "$REGISTRY_URL" ]; then
+    REGISTRY_URL="$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')"
+fi
+
+usage() {
+echo "Usage:  $(basename "$0") [MODE] [OPTIONS] [COMMAND]"
+echo 
+echo "Mode:"
+echo
+echo "Options:"
+echo "  --help          Show this help message"
+echo
+echo "Commands:"
+echo "  up              Start the services"
+echo "  down            Stop the services"
+echo "  ps              Show the status of the services"
+echo "  logs            Follow the logs on console"
+echo "  remove-all      Remove all containers"
+echo "  stop-all        Stop all containers running"
+echo "  build           Build the image"
+echo "  publish         Publish the image to the registry"
+}
+
+CONF_ARG="-f docker-compose.yml"
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+for i in "$@"
+do
+case $i in
+    --help|-h)
+        usage
+        exit 1
+        ;;
+    *)
+        ;;
+esac
+done
+
+echo "Arguments: $CONF_ARG"
+echo "Command: $@"
+
+if [ "$1" == "up" ]; then
+    docker-compose $CONF_ARG pull
+    docker-compose $CONF_ARG build --pull
+    docker-compose $CONF_ARG up -d --remove-orphans
+    exit 0
+
+elif [ "$1" == "stop-all" ]; then
+    if [ -n "$(docker ps --format {{.ID}})" ]
+    then docker stop $(docker ps --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "remove-all" ]; then
+    if [ -n "$(docker ps -a --format {{.ID}})" ]
+    then docker rm $(docker ps -a --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "logs" ]; then
+    shift
+    docker-compose $CONF_ARG logs -f --tail 200 "$@"
+    exit 0
+
+fi
+
+docker-compose $CONF_ARG "$@"
